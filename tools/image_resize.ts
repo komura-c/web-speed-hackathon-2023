7acefef08932993c0101aede660ba012ad4e8600
komura-c <affirman03139@outlook.jp>
2023-03-05 16:45:21 +0900

    fix: webp

--- tools/image_resize.ts
+++ tools/image_resize.ts
@@ -1,5 +1,5 @@
-// import fg from 'fast-glob';
-// import sharp from 'sharp';
+import fg from 'fast-glob';
+import sharp from 'sharp';
 
 // fg('./public/images/avatars/*.jpg').then((strings)=>{
 //   strings.map(async path => {
@@ -10,11 +10,11 @@
 //   })
 // })
 
-// fg('./public/images/products/tomato/*.jpg').then((strings)=>{
-//   strings.map(async path => {
-//     await sharp(path)
-//       .resize(480, 240)
-//       .png({ quality: 30 })
-//       .toFile('./compressed' + path.slice(1));
-//   })
-// })
+fg('./public/images/avatars/*.jpg').then((strings)=>{
+  strings.map(async (path,index) => {
+    await sharp(path)
+      .resize(104, 104)
+      .webp({ quality: 30 })
+      .toFile('./public/compressed/avatars/' + String(index+1).padStart(3,'0') + '.webp');
+  })
+})
